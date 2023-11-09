daf00a2f0280699c9ee047356187d9dd6aa487f9
igunereve <igunereve@gmail.com>
2023-11-10 01:30:30 +0200

    path change

--- src/data.js
+++ src/data.js
@@ -5,9 +5,9 @@ export default [
     description:
       'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
-    coverImg: "/public/images/katie-zaferes.png",
+    coverImg: "/images/katie-zaferes.png",
     stats: {
-      starImage: "/public/images/star.png",
+      starImage: "/images/star.png",
       rating: 5.0,
       reviewCount: 6,
     },
@@ -20,9 +20,9 @@ export default [
     description:
       "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
     price: 125,
-    coverImg: "/public/images/wedding-photography.png",
+    coverImg: "/images/wedding-photography.png",
     stats: {
-      starImage: "/public/images/star.png",
+      starImage: "/images/star.png",
       rating: 5.0,
       reviewCount: 30,
     },
@@ -35,9 +35,9 @@ export default [
     description:
       "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
     price: 50,
-    coverImg: "/public/images/mountain-bike.png",
+    coverImg: "/images/mountain-bike.png",
     stats: {
-      starImage: "/public/images/star.png",
+      starImage: "/images/star.png",
       rating: 4.8,
       reviewCount: 2,
     },
